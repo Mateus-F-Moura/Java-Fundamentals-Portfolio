e88f87ad3d72c1b30b1278f8b279b2c5dfb7a34e
Mateus-F-Moura <mf753161@gmail.com>
2025-11-18 22:17:36 -0300

    using vectors on classes

---
+++ src/Arrays/Product/application/Main.java
@@ -0,0 +1,40 @@
+package Arrays.Product.application;
+
+import Arrays.Product.entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        try (Scanner sc = new Scanner(System.in)) {
+
+            int n = sc.nextInt();
+
+            Product[] vector = new Product[n];
+
+            for (int i = 0; i < vector.length; i++) {
+                sc.nextLine();
+
+                String name = sc.nextLine();
+                int quantity = sc.nextInt();
+                double price = sc.nextDouble();
+
+                vector[i] = new Product(name, quantity, price);
+            }
+
+            double sum = 0.0;
+            for (int i = 0; i < vector.length; i++) {
+                sum += vector[i].getPrice();
+            }
+
+            double average = sum / vector.length;
+
+            System.out.println("Average: " + average);
+
+        }
+    }
+
+}
