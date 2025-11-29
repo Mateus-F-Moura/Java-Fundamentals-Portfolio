13ee12a5726486d678cfe3ea0ee7cac9a55a568f
Mateus-F-Moura <mf753161@gmail.com>
2025-11-29 01:56:09 -0300

    Older people with arrays

---
+++ src/Arrays/Age/application/Main.java
@@ -0,0 +1,45 @@
+package Arrays.Age.application;
+
+import Arrays.Age.entities.Person;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        try (Scanner sc = new Scanner(System.in)) {
+
+            System.out.print("How many people add: ");
+            int n = sc.nextInt();
+            sc.nextLine();
+
+            Person[] people = new Person[n];
+
+            for (int i = 0; i < people.length; i++) {
+                System.out.println("== Enter the person number " + i + " ==");
+
+                System.out.print("Name: ");
+                String name = sc.nextLine();
+
+                System.out.print("Age: ");
+                int age = sc.nextInt();
+                sc.nextLine();
+
+                people[i] = new Person(name, age);
+            }
+
+            String olderName = people[0].getName();
+            int olderAge = people[0].getAge();
+            for (Person person : people) {
+                if (person.getAge() > olderAge) {
+                    olderName = person.getName();
+                    olderAge = person.getAge();
+                }
+            }
+
+            System.out.println("Older person: " + olderName + " with " + olderAge + "y old");
+
+        }
+    }
+}
